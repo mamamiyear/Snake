789c39d3db05382edf883a3fba02792128587e0c
mamamiyear <mamamiyear@163.com>
2016-01-04 17:22:55 +0800

    Add Atom 1-4 17:20

---
+++ src/backstage/Snake.java
@@ -0,0 +1,15 @@
+package backstage;
+
+import gui.GameAtom;
+
+import java.util.LinkedList;
+
+/**
+ * @author mamamiyear
+ *         on 16-1-4.
+ */
+public class Snake extends LinkedList<GameAtom> {
+
+
+
+}
--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -9,11 +9,14 @@ import java.awt.*;
  */
 public class MainFrame extends JFrame {
 
-    private Container container;
+    private JPanel container;
+
     private JPanel gamePane;
     private JPanel ctrlPane;
-    private int screenWidth;
-    private int screenHeight;
+    private JButton startBtn;
+    private JButton stopBtn;
+    private JButton restartBtn;
+    private JButton pauseBtn;
 
 
     public MainFrame() {
@@ -27,25 +30,51 @@ public class MainFrame extends JFrame {
 
     private void initFrame() {
 
-        screenWidth = Toolkit.getDefaultToolkit().getScreenSize().width;
-        screenHeight = Toolkit.getDefaultToolkit().getScreenSize().height;
-        if(screenWidth <= 450 || screenHeight <= 350) System.exit(0);
-        container = this.getContentPane();
-        container.setLayout(new FlowLayout());
+
+        int screenWidth = Toolkit.getDefaultToolkit().getScreenSize().width;
+        int screenHeight = Toolkit.getDefaultToolkit().getScreenSize().height;
+        container = new JPanel();
+        container.setPreferredSize(new Dimension(500, 350));
+        container.setSize(new Dimension(500, 350));
+        container.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
+        this.add(container);
+        this.pack();
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setLocation((screenWidth - this.getWidth()) / 2, (screenHeight - this.getHeight()) / 2);
     }
 
     private void initComponents() {
 
         gamePane = new JPanel();
         ctrlPane = new JPanel();
+        startBtn = new JButton("开始");
+        stopBtn = new JButton("停止");
+        restartBtn = new JButton("重新开始");
+        pauseBtn = new JButton("暂停");
+
+        startBtn.setPreferredSize(new Dimension(100, 30));
+        stopBtn.setPreferredSize(new Dimension(100, 30));
+        restartBtn.setPreferredSize(new Dimension(100, 30));
+        pauseBtn.setPreferredSize(new Dimension(100, 30));
 
         gamePane.setPreferredSize(new Dimension(350, 350));
         gamePane.setSize(new Dimension(350, 350));
         gamePane.setBackground(Color.black);
-        ctrlPane.setBackground(Color.blue);
+
+        ctrlPane.setPreferredSize(new Dimension(150, 350));
+        ctrlPane.setSize(new Dimension(150, 350));
+        ctrlPane.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 46));
+
+        ctrlPane.add(startBtn);
+        ctrlPane.add(stopBtn);
+        ctrlPane.add(pauseBtn);
+        ctrlPane.add(restartBtn);
 
         container.add(gamePane);
         container.add(ctrlPane);
+
+
 
     }
 
